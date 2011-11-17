62ff39685d174de4bce5dc6e3f6bf2e837209402
Emilio López <turl@tuxfamily.org>
2011-11-17 05:47:23 +0000

    Reintroduce BOARD_VOLD_EMMC_SHARES_DEV_MAJOR
    Code taken from CM7's vold
    
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -58,6 +58,10 @@ LOCAL_C_INCLUDES := $(common_c_includes)
 
 LOCAL_CFLAGS := -Werror=format
 
+ifeq ($(BOARD_VOLD_SHARES_DEV_MAJOR), true)
+LOCAL_CFLAGS += -DSHARES_DEV_MAJOR
+endif
+
 LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
 
 LOCAL_STATIC_LIBRARIES := libfs_mgr
